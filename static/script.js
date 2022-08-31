f35dbe2711aca6b523a8f1da8a86a9c5fe944f40
eszterberenyi <e.eszterberenyi@gmail.com>
2022-08-31 14:49:41 +0200

    add basic modal style and close and open events

--- static/script.js
+++ static/script.js
@@ -1,23 +1,37 @@
-const tableBody = document.querySelector('tbody');
+const planetsTableBody = document.querySelector('#planets-tbody');
 const listDiv = document.querySelector('#small-window-list');
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
+const closeIcon = document.querySelector('.close');
+const closeButton = document.querySelector('#close-modal-btn');
+const modal = document.querySelector('#residents-modal');
+const modalTableBody = document.querySelector('#modal-table-body');
 
 nextPrevButtons(prev);
 nextPrevButtons(next);
 
-window.addEventListener('resize', function(event){
+
+window.addEventListener('resize', function (event) {
     if (window.innerWidth < 990) {
         listDiv.classList.remove('inactive');
-        document.querySelector('#my-table').classList.add('inactive')
+        document.querySelector('#planets-table').classList.add('inactive')
     } else {
         listDiv.classList.add('inactive');
-        document.querySelector('#my-table').classList.remove('inactive')
+        document.querySelector('#planets-table').classList.remove('inactive')
     }
 });
 
 
-async function getJSON(pageNum=1) {
+closeIcon.addEventListener('click', () => modal.classList.add('inactive'));
+closeButton.addEventListener('click', () => modal.classList.add('inactive'));
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.add('inactive');
+    }
+})
+
+
+async function getJSON(pageNum = 1) {
     const response = await fetch(
         `https://swapi.py4e.com/api/planets?page=${pageNum}`,
         {
@@ -31,8 +45,7 @@ async function getJSON(pageNum=1) {
 function checkLength(arr, buttonType) {
     if (arr.length !== 0 && buttonType === 'residents') {
         return `<button type="button" className="btn btn-outline-dark residents-btn" data-mdb-ripple-color="dark">${arr.length} resident(s)</button>`;
-    }
-    else if (arr.length !== 0 && buttonType === 'vote') {
+    } else if (arr.length !== 0 && buttonType === 'vote') {
         return `<button class="residents-btn">${arr.length} resident(s)</button>`
     } else {
         return 'No known residents';
@@ -45,7 +58,7 @@ function buildPage(entries) {
         for (let planet of entries.results) {
             let tr = document.createElement('tr');
             let pDiv = document.createElement('div');
-            tableBody.appendChild(tr);
+            planetsTableBody.appendChild(tr);
             listDiv.appendChild(pDiv)
             tr.innerHTML = `
             <td>${planet.name}</td>
@@ -54,7 +67,7 @@ function buildPage(entries) {
             <td>${planet.terrain}</td>
             <td>${planet.surface_water !== 'unknown' ? planet.surface_water + '%' : 'unknown'}</td>
             <td>${planet.population !== 'unknown' ? Number(planet.population).toLocaleString() + ' people' : 'unknown'}</td>      
-            <td>${checkLength(planet.residents, 'residents')}</td> 
+            <td class="residents-btnTd">${checkLength(planet.residents, 'residents')}</td> 
             <td><button type="button" className="btn btn-outline-dark vote-btn" data-mdb-ripple-color="dark">Vote</button></td>     
             `
             pDiv.innerHTML = `
@@ -64,11 +77,17 @@ function buildPage(entries) {
             <p>${planet.terrain}</p>
             <p>${planet.surface_water !== 'unknown' ? planet.surface_water + '%' : 'unknown'}</p>
             <p>${planet.population !== 'unknown' ? Number(planet.population).toLocaleString() + ' people' : 'unknown'}</p>
-            <p>${checkLength(planet.residents, 'vote')}</p>
+            <p class="residents-btnTd">${checkLength(planet.residents, 'vote')}</p>
             <p><button class="vote-btn">Vote</button></p>
             <p></p>
             `
         }
+        document.querySelectorAll('.residents-btnTd>button').forEach((x) => {
+            x.addEventListener('click', (e) => {
+                console.log(e);
+                modal.classList.remove('inactive');
+            })
+        })
     } catch (error) {
         return;
     }
@@ -82,24 +101,34 @@ getJSON().then(entries => {
 
 function nextPrevButtons(type) {
     type.addEventListener('click', (e) => {
-    if(e.detail > 1) {
-        return;
-    } else {
-        let pageNum = parseInt(type.dataset.page);
-        if (type === prev) {
-            pageNum -= 1;
+        if (e.detail > 1) {
+            return;
         } else {
-            pageNum += 1;
-        }
-        getJSON(pageNum).then(entries => {
-            if (entries.detail !== 'Not found') {
-                tableBody.textContent = '';
-                listDiv.textContent = '';
-                prev.dataset.page = pageNum.toString();
-                next.dataset.page = pageNum.toString();
-                buildPage(entries)
+            let pageNum = parseInt(type.dataset.page);
+            if (type === prev) {
+                pageNum -= 1;
+                if (pageNum === 1) {
+                    prev.classList.add('inactive');
+                } else if (pageNum !== 7) {
+                    next.classList.remove('inactive');
+                }
+            } else {
+                pageNum += 1;
+                if (pageNum === 7) {
+                    next.classList.add('inactive');
+                } else if (pageNum !== 1) {
+                    prev.classList.remove('inactive');
+                }
             }
-        })
-    }
-})
+            getJSON(pageNum).then(entries => {
+                if (entries.detail !== 'Not found') {
+                    planetsTableBody.textContent = '';
+                    listDiv.textContent = '';
+                    prev.dataset.page = pageNum.toString();
+                    next.dataset.page = pageNum.toString();
+                    buildPage(entries)
+                }
+            })
+        }
+    })
 }
